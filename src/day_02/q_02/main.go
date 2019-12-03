59389698968a4ac9e0742442f2d90d417a5aee89
Archimedyz <awais.ali1248@gmail.com>
2019-12-02 20:59:08 -0500

    Finish Day 2, Question 2

---
+++ src/day_02/q_02/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func main() {
+	target := 19690720
+
+	for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			output := IntCodeRun(noun, verb)
+
+			if target == output {
+				fmt.Printf("output: %d\n", (100*noun)+verb)
+			}
+		}
+	}
+}
+
+func IntCodeRun(noun int, verb int) int {
+	memory := []int{1, noun, verb, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 2, 6, 19, 23, 1, 23, 5, 27, 1, 27, 13, 31, 2, 6, 31, 35, 1, 5, 35, 39, 1, 39, 10, 43, 2, 6, 43, 47, 1, 47, 5, 51, 1, 51, 9, 55, 2, 55, 6, 59, 1, 59, 10, 63, 2, 63, 9, 67, 1, 67, 5, 71, 1, 71, 5, 75, 2, 75, 6, 79, 1, 5, 79, 83, 1, 10, 83, 87, 2, 13, 87, 91, 1, 10, 91, 95, 2, 13, 95, 99, 1, 99, 9, 103, 1, 5, 103, 107, 1, 107, 10, 111, 1, 111, 5, 115, 1, 115, 6, 119, 1, 119, 10, 123, 1, 123, 10, 127, 2, 127, 13, 131, 1, 13, 131, 135, 1, 135, 10, 139, 2, 139, 6, 143, 1, 143, 9, 147, 2, 147, 6, 151, 1, 5, 151, 155, 1, 9, 155, 159, 2, 159, 6, 163, 1, 163, 2, 167, 1, 10, 167, 0, 99, 2, 14, 0, 0}
+
+	//fmt.Printf("input:  %v\n", memory)
+
+	for i := 0; i < cap(memory); i += 4 {
+		opcode := memory[i]
+
+		//fmt.Printf("opcode [%d] at index [%d]\n", opcode, i)
+
+		if opcode == 99 {
+			break
+		}
+
+		if i+4 > cap(memory) {
+			fmt.Println("Invalid input! Not enough values in the look-ahead.")
+			return -1
+		}
+
+		index_1 := memory[i+1]
+		index_2 := memory[i+2]
+		index_3 := memory[i+3]
+
+		if opcode == 1 {
+			memory[index_3] = memory[index_1] + memory[index_2]
+			continue
+		}
+
+		if opcode == 2 {
+			memory[index_3] = memory[index_1] * memory[index_2]
+			continue
+		}
+
+		fmt.Printf("Invalid opcode (%d) at index: %d!\n", opcode, i)
+		return -1
+	}
+
+	// fmt.Printf("output: %v\n", memory)
+
+	return memory[0]
+}
